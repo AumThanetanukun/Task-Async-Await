1b62aba091ef66ed313094d96f832a89494abdee
Aum Thanetanukun <chaloem.tha@outlook.com>
2019-04-12 11:36:01 +0700

    MultiTask

--- AsyncAwait/AsyncAwait.cs
+++ AsyncAwait/AsyncAwait.cs
@@ -10,29 +10,54 @@ namespace AsyncAwait
     {
         static void Main(string[] args)
         {
-            TestComplete();
+            var num = new List<int>()
+            {
+                10,20,30,40,50
+            };
+            TestComplete(5, num);
             Console.ReadKey();
         }
 
-        static async void TestComplete()
+        static async void TestComplete(int n, List<int> num)
         {
             Console.WriteLine("Line 1");
-            Task<int> t = new Task<int>(() => ComplexCalculation());
-            t.Start();
+            List<Task> t = new List<Task>();
+
+            foreach (var item in num)
+            {
+                var task = new Task<int>(() => ComplexCalculation(item));
+                Console.WriteLine("Item value: " + item);
+                task.Start();
+                t.Add(task);
+            }
+
             Console.WriteLine("Line 2");
             Console.WriteLine("Line 3");
-            var result = await t;
-            Console.WriteLine(result);
+            
+            //await Task.WhenAll(t.ToArray());
+            Task.WaitAll(t.ToArray());
+            
+            foreach (Task<int> task in t)
+            {
+                // Not Need Async Keyword
+                //var result = task.Result;
+
+                var result = await task;
+                Console.WriteLine("Result --- " + result);
+            }
+
             Console.WriteLine("Line 4");
             Console.WriteLine("Line 5");
         }
 
-        static int ComplexCalculation()
+        static int ComplexCalculation(int n)
         {
-            double x = 2;
-            for (int i = 0; i < 100000000; i++)
-                x += Math.Sqrt(x) / i;
-            return (int)x;
+            /*double x = 2;
+            int num = n * 1000000;
+            for (int i = 0; i < num; i++)
+                x += Math.Sqrt(x) / i;*/
+            Console.WriteLine("In Calculation : " + n);
+            return (int)n;
         }
     }
 }
